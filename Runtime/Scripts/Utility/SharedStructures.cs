db7161a4c5f1b4aa64b346a69c8d4b6b0ba0f788
Yakvi <ivankoy@gmail.com>
2022-11-27 15:21:56 +0100

    Add variable cached data

--- Runtime/Scripts/Utility/SharedStructures.cs
+++ Runtime/Scripts/Utility/SharedStructures.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace VenetStudio
@@ -6,12 +7,47 @@ namespace VenetStudio
     {
         public T value;
         private int cacheSnapshot = -1;
-        public bool IsRelevant()
+        public bool IsFresh()
         {
             var time = Time.frameCount;
             if (cacheSnapshot == time) return true;
             cacheSnapshot = time;
             return false;
+        }
+    }
+    
+    public class CachedData<TIndex, T>
+    {
+        public Dictionary<TIndex, Data> values;
+
+        public T GetValue(TIndex index)
+        {
+            if (values == null) return default;
+            if (!values.ContainsKey(index)) return default;
+            return values[index].value;
+        }
+
+        public void SetValue(TIndex index, T value)
+        {
+            values ??= new Dictionary<TIndex, Data>();
+            values[index] = new Data
+            {
+                snapshot = Time.frameCount,
+                value = value
+            };
+        }
+        
+        public bool IsFresh(TIndex index)
+        {
+            values ??= new Dictionary<TIndex, Data>();
+            if (!values.ContainsKey(index)) return false;
+            return values[index].snapshot == Time.frameCount;
+        }
+
+        public struct Data
+        {
+            public int snapshot;
+            public T value;
         }
     }
 }
