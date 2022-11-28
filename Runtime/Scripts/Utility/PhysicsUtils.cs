008b97c0d70d1b2e600388dcd99e6d3cc315530a
Yakvi <ivankoy@gmail.com>
2022-11-28 08:52:38 +0100

    Update physics package to include raycast hit, allocating overlap

--- Runtime/Scripts/Utility/PhysicsUtils.cs
+++ Runtime/Scripts/Utility/PhysicsUtils.cs
@@ -6,14 +6,17 @@ namespace VenetStudio
 {
     public partial class Utility
     {
-        public static bool Raycast(Vector3 position, Vector3 direction, float maxDistance, LayerMask mask) =>
-            Physics.Raycast(position, direction, maxDistance, mask);
+        public static bool Raycast(Vector3 position, Vector3 direction, out RaycastHit hit, float maxDistance, LayerMask mask) =>
+            Physics.Raycast(position, direction, out hit, maxDistance, mask);
 
         public static bool Raycast(Ray ray, out RaycastHit hit, float maxDistance, LayerMask mask) =>
             Physics.Raycast(ray, out hit, maxDistance, mask);
 
         public static int OverlapSphere(Vector3 center, float radius, Collider[] results, int mask = ~0) =>
             Physics.OverlapSphereNonAlloc(center, radius, results, mask);
+        
+        public static Collider[] OverlapSphere(Vector3 center, float radius, int mask = ~0) =>
+            Physics.OverlapSphere(center, radius, mask);
     }
 
 }
